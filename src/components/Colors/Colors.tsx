a1c0734bb5371ef772f5ede02ecc0b6fffe04b70
Seweryn Karecki <sewerynkarecki@gmail.com>
2022-06-03 12:27:42 +0100

    logic implemented for styles,fonts,colors

--- src/components/Colors/Colors.tsx
+++ src/components/Colors/Colors.tsx
@@ -5,31 +5,84 @@ import Friendly from '../../assets/img/Colors_palletes/Friendly.svg'
 import Intense from '../../assets/img/Colors_palletes/Intense.svg'
 import Natural from '../../assets/img/Colors_palletes/Natural.svg'
 import Somber from '../../assets/img/Colors_palletes/Somber.svg'
-
+import { useDispatch } from 'react-redux'
+import { setColor } from '../../store/slices/logoSlice/logoSlice'
+ 
 const Colors = () => {
+let id = null;
+  const dispatch = useDispatch();
+
   return (
     <div className="flex flex-col h-full w-full p-1">
       <h2 className="text-3xl text-center font-bold p-4"> Choose a layout</h2>
       <div className=" flex flex-row justify-evenly my-30">
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Friendly} alt="layout1" />
+          <img 
+          className="hover:bg-green-500 p-1 rounded-lg"
+          id='1'
+           src={Friendly}
+            alt="layout1"
+           onClick={() => {
+              id = 1
+              dispatch(setColor(id))
+              console.log(id)}}/>
         </div>
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Natural} alt="layout2" />
+          <img 
+          className="hover:bg-green-500 p-1 rounded-lg"
+          id='2'
+           src={Natural}
+            alt="layout2"
+           onClick={() => {
+              id = 2
+              dispatch(setColor(id))
+              console.log(id)}}/>
             </div>
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Bold} alt="layout4" />
+          <img 
+          className="hover:bg-green-500 p-1 rounded-lg"
+          id='3'
+           src={Bold} 
+            alt="layout4"
+           onClick={() => {
+              id = 3
+              dispatch(setColor(id))
+              console.log(id)}}/>
         </div>
       </div>
       <div className=" flex flex-row justify-evenly py-10">
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Intense} alt="layout3" />
+          <img 
+          className="hover:bg-green-500 p-1 rounded-lg"
+          id='4'
+           src={Intense}
+            alt="layout3"
+          onClick={() => {
+              id = 4
+              dispatch(setColor(id))
+              console.log(id)}}/>
         </div>
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Somber} alt="layout4" />
+          <img 
+          className="hover:bg-green-500 p-1 rounded-lg"
+          id='5'
+           src={Somber}
+            alt="layout4"
+          onClick={() => {
+              id = 5
+              dispatch(setColor(id))
+              console.log(id)}}/>
         </div>
          <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Cheerful} alt="layout4" />
+          <img 
+          className="hover:bg-green-500 p-1 rounded-lg"
+          id='6'
+           src={Cheerful}
+            alt="layout4"
+          onClick={() => {
+              id = 6
+              dispatch(setColor(id))
+              console.log(id)}}/>
         </div>
       </div>
     </div>
--- src/components/Fonts/Fonts.tsx
+++ src/components/Fonts/Fonts.tsx
@@ -8,41 +8,118 @@ import Handwritten from '../../assets/img/Fonts/Handwritten.svg'
 import Luxury from '../../assets/img/Fonts/Luxury.svg'
 import Modern from '../../assets/img/Fonts/Modern.svg'
 import Rounded from '../../assets/img/Fonts/Rounded.svg'
+import { useDispatch } from 'react-redux'
+import { setFont } from '../../store/slices/logoSlice/logoSlice'
+
 const Fonts = () => {
+  const dispatch = useDispatch();
+  let id = null;
   return (
     <div className="flex flex-col h-full w-full p-1">
       <h2 className="text-3xl text-center font-bold p-4"> Choose a layout</h2>
       <div className=" flex flex-row justify-evenly my-30">
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Clean} alt="layout1" />
+          <img 
+            id='1'
+             onClick={() => {
+              id=1
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Clean} alt="layout1" />
         </div>
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Modern} alt="layout2" />
+          <img 
+            id='2'
+            onClick={() => {
+              id=2
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Modern} alt="layout2" />
             </div>
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Funny} alt="layout4" />
+          <img 
+            id='3'
+            onClick={() => {
+              id=3
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Funny} alt="layout4" />
         </div>
       </div>
       <div className=" flex flex-row justify-evenly py-10">
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Luxury} alt="layout3" />
+          <img 
+            id='4'
+            onClick={() => {
+              id=4
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Luxury} alt="layout3" />
         </div>
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Handwritten} alt="layout4" />
+          <img
+            id='5'
+            onClick={() => {
+              id=5
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Handwritten} alt="layout4" />
         </div>
          <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Geometric} alt="layout4" />
+          <img 
+            id='6'
+            onClick={() => {
+              id=6
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Geometric} alt="layout4" />
         </div>
         </div>
          <div className=" flex flex-row justify-evenly py-10">
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Rounded} alt="layout3" />
+          <img 
+            id='7'
+            onClick={() => {
+              id=7
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Rounded} alt="layout3" />
         </div>
         <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Futuristic} alt="layout4" />
+          <img 
+            id='8'
+            onClick={() => {
+              id=8
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Futuristic} alt="layout4" />
         </div>
          <div className="">
-          <img className="hover:bg-green-500 p-1 rounded-lg" src={Funky} alt="layout4" />
+          <img 
+            id='9'
+            onClick={() => {
+              id=9
+              console.log(id)
+              dispatch(setFont(id))
+            }}
+          className="hover:bg-green-500 p-1 rounded-lg"
+           src={Funky} alt="layout4" />
         </div>
       </div>  
     </div>
