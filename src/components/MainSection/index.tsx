d348d4e93b3b59d3c29861e341e0323e321d0725
Seweryn Karecki <sewerynkarecki@gmail.com>
2022-06-06 07:23:31 +0100

    create main section in editor page

---
+++ src/components/MainSection/index.tsx
@@ -0,0 +1,8 @@
+const MainSection: React.FC = () => {
+  return (
+    <>
+      <div className="border h-full mt-15">MainSection</div>
+    </>
+  )
+}
+export default MainSection
--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -1,9 +1,11 @@
+import MainSection from '../../components/MainSection'
 import Navbar from '../../components/NavBar'
 
 const EditorPage: React.FC = () => {
   return (
     <>
       <Navbar />
+      <MainSection />
     </>
   )
 }
