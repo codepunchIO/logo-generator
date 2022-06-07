b4cd78c92a99ea1bc57ea4d8cefbefeed302b93b
Seweryn Karecki <sewerynkarecki@gmail.com>
2022-06-07 14:53:22 +0100

    setting chosen color as font background

--- src/components/Fonts/Fonts.tsx
+++ src/components/Fonts/Fonts.tsx
@@ -9,13 +9,62 @@ import { store } from "../../store/store";
 import { common } from "@mui/material/colors";
 
 
+const colorCards = [
+  {
+    title: "Friendly, Loyal & Peacefull",
+    colors: ["bg-blue-400", "bg-blue-300", "bg-blue-200", "bg-blue-100"],
+  },
+  {
+    title: "Natural, Happy & Calm",
+    colors: ["bg-green-400", "bg-green-300", "bg-green-200", "bg-green-100"],
+  },
+  {
+    title: "Bold, Engaging & Vibrant",
+    colors: [
+      "bg-purple-400",
+      "bg-purple-300",
+      "bg-purple-200",
+      "bg-purple-100",
+    ],
+  },
+  {
+    title: "Intense, Warm & Romantic",
+    colors: ["bg-red-400", "bg-red-300", "bg-red-200", "bg-red-100"],
+  },
+  {
+    title: "Somber, Earthy,Solid & Natural",
+    colors: [
+      "bg-orange-400",
+      "bg-orange-300",
+      "bg-orange-200",
+      "bg-orange-100",
+    ],
+  },
+  {
+    title: "Cheerful, Vibrant & Happy",
+    colors: ["bg-amber-400", "bg-amber-300", "bg-amber-200", "bg-amber-100"],
+  },
+];
 
 const Fonts = () => {
 
   const state = store.getState();
   const [familiesNames, setfamiliesNames] = useState<any>(null);
   const [selectedQuery, setQuery] = useState<string>('TRENDING');
+
+  const [pallete,setPallete] = useState<string>('bg-white-500')
+ 
+  useEffect(()=>{
+    if (state.logo.data.colors ) {
+      const pallete_id = state.logo.data.colors! - 1;
+      setPallete(colorCards[pallete_id].colors[2])
+    }
+    else {
+      setPallete('bg-white-500')
+    }
+    },[])
 //  let isLoaded = false;
+  
   
   //possible queries
   const fetchData = async () => {
@@ -124,10 +173,10 @@ const Fonts = () => {
             familiesNames?
               familiesNames.map((family: any) => (
               (state.logo.data.brandName!=='')?(
-                  <div className="flex flex-row w-1/3 flex-nowrap hover:border-2 hover:border-green-500 h-24  justify-center p-3 "
+                  <div className=' flex flex-row w-1/3 flex-nowrap hover:border-2 hover:border-green-500 h-24  justify-center p-3 '
                   key={familiesNames.indexOf(family)}>
                 <span style={{ fontFamily: `${family}` }}
-                  className=' flex flex-col text-center justify-center h-auto text-2xl'
+                  className={`${pallete} flex flex-col text-center justify-center h-auto text-2xl px-6 py-2`}
                       onClick={(e) => handleClick(e)}
                       title={family}
                 >
@@ -137,10 +186,10 @@ const Fonts = () => {
                </div>
                 ) :
                 (
-              <div className="flex flex-row w-1/3 flex-nowrap hover:border-2 hover:border-green-500 h-24  justify-center p-3 " 
+              <div className=' flex flex-row w-1/3 flex-nowrap hover:border-2 hover:border-green-500 h-24  justify-center p-3 '
               key={familiesNames.indexOf(family)} >
                 <span style={{ fontFamily: `${family}` }}
-                  className=' flex flex-col text-center justify-center h-auto text-2xl'
+                       className={`${pallete} flex flex-col w-5/6  text-center justify-center h-auto text-2xl px-6 py-2`}
                         onClick={(e) => handleClick(e)}
                         title={family}
                 >
