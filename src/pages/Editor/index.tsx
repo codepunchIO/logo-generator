65439c9b65302c145c5562a6ffb876ca3da6b164
Seweryn Karecki <sewerynkarecki@gmail.com>
2022-06-07 18:59:23 +0100

    in progress editor page

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -1,12 +1,15 @@
+import { Toolbar } from '@mui/material'
 import MainSection from '../../components/MainSection'
 import Navbar from '../../components/NavBar'
 
 const EditorPage: React.FC = () => {
   return (
-    <>
+    <div className="h-screen">
       <Navbar />
-      <MainSection />
-    </>
+      <main className="border border-sky-500 h-5/6">
+        <MainSection />
+      </main>
+    </div>
   )
 }
 export default EditorPage
